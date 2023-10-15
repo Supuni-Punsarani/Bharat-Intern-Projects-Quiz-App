e89b6c696c85cb555cb0af48a4b74b5aa559524c
Supuni Wannigma <114279717+Supuni-Punsarani@users.noreply.github.com>
2023-10-15 11:35:55 +0530

    Add files via upload

---
+++ UserData.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package quiz.app;
+
+/**
+ *
+ * @author S.PUNSARANI
+ */
+public class UserData {
+    private static String name;
+
+    public static String getName() {
+        return name;
+    }
+
+    public static void setName(String newName) {
+        name = newName;
+    }
+}
